4561aa785e67a65b7751c452ab7999a04aa60b7d
Developerlog <mhy9203.develop@gmail.com>
2026-04-17 02:18:35 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDEKi9U8mt2Z8w0n3OrPKBFVxJwvzGXpcI",
-  authDomain: "tchoukball-462b5.firebaseapp.com",
-  projectId: "tchoukball-462b5",
-  storageBucket: "tchoukball-462b5.appspot.com",
-  messagingSenderId: "630695302210",
-  appId: "1:630695302210:web:890d058be4bffd220c09db"
+  // plz use .env to secure your personal data and block abusing
+  // 선생님 이렇게 두시면 선생님 firebase로 아무나 데이터 올리고 주고받고할 수 있어서 나중에 필요할때 사용량 제한 걸릴 수도 있고 Blaze요금제 구독중이신경우 그 금액만큼 청구되실수 있어서 .env를 사용하시는것을 추천드리비다! :)
+  // 그리고 지금 저희 동아리에서 멤버 전부 확정되어서 이제 개발 시작했습니다! 저희가 개발하고 있는 리포지토리는 https://github.com/backpropagation-jh/tchoukball_judger 입니다! 감사합니다 :)
+  // 중흥중학교 2학년 2반 10번 올림
 };
 
 const app = initializeApp(firebaseConfig);
